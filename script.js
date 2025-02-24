8b2f620b92e652f67a6362ab810cddd4d9311168
Vaibhav Sain <155076216+VaibhavSain@users.noreply.github.com>
2025-02-24 17:30:40 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+box1 = document.querySelector(".hide");
+noti = document.querySelector(".noti");
+main = document.querySelector("main");
+function remove() {
+    box1.style.display = "none";
+    noti.innerText = "0";
+    main.style.height = "8rem";
+
+}
